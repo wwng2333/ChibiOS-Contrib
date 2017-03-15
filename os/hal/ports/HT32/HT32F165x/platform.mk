aba9e268bb0c68c2ef721fd6877c36a9fc6f32f1
Charlie Waters <cawiii@me.com>
2017-03-14 23:10:12 -0400

    Adding ht32 templates

---
+++ os/hal/ports/HT32/HT32F165x/platform.mk
@@ -0,0 +1,11 @@
+# List of all the template platform files.
+PLATFORMSRC = ${CHIBIOS}/os/hal/ports/common/ARMCMx/nvic.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/HT32/HT32F165x/hal_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/HT32/HT32F165x/hal_gpt_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/HT32/HT32F165x/hal_pal_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/HT32/HT32F165x/hal_spi_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/HT32/HT32F165x/hal_usb_lld.c
+
+# Required include directories
+PLATFORMINC = ${CHIBIOS}/os/hal/ports/common/ARMCMx \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/HT32/HT32F165x
