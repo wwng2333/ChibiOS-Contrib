0086bb760019168c29318510694b5f6ee39bb93e
wwng2333 <wwng@2333.me>
2020-03-11 10:50:34 +0800

    add nuc121 parameters and startup

---
+++ os/common/startup/ARMCMx/compilers/GCC/mk/startup_nuc121.mk
@@ -0,0 +1,18 @@
+# List of the ChibiOS generic NuMicro NUC121 startup and CMSIS files.
+STARTUPSRC = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt1.c
+
+STARTUPASM = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt0_v6m.S \
+             $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/vectors.S
+
+STARTUPINC = $(CHIBIOS)/os/common/portability/GCC \
+             $(CHIBIOS)/os/common/ext/ARM/CMSIS/Core/Include \
+             $(CHIBIOS_CONTRIB)/os/common/startup/ARMCMx/devices/NUC121 \
+             $(CHIBIOS_CONTRIB)/os/common/ext/CMSIS/NUMICRO
+
+STARTUPLD  = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/ld
+STARTUPLD_CONTRIB  = $(CHIBIOS_CONTRIB)/os/common/startup/ARMCMx/compilers/GCC/ld
+
+# Shared variables
+ALLXASMSRC += $(STARTUPASM)
+ALLCSRC    += $(STARTUPSRC)
+ALLINC     += $(STARTUPINC)
